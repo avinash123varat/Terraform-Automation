9858ee3bf7511889bda6bda7a7ed18fe75306ca2
avinash123varat <116380049+avinash123varat@users.noreply.github.com>
2022-12-12 13:18:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "avikey" 
+    default     =  "linux" 
 }
 
 variable "instance_type" { 
